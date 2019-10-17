5c37a8dd98d33c53bb321ddf904f5bc49bc49b80
Diwas KC <diwaskc12@gmail.com>
2019-10-17 08:43:37 +0545

    Initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="text-gray-700 text-sm leading-normal flex flex-wrap justify-between mx-auto max-w-3xl px-6 sm:px-12">
+    <div class="w-full sm:w-1/2 mb-4 sm:mb-0">
+    </div>
+    <div class="w-full sm:w-1/2">
+      <nav>
+        <ul class="flex sm:justify-end -mx-2">
+          <li class="px-2">
+            <g-link to="/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Home</g-link>
+          </li>
+          <li class="px-2">
+            <g-link to="/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Gallery</g-link>
+          </li>
+          <li class="px-2">
+            <g-link to="/Cv" class="border-b border-transparent hover:border-gray-400 transition-border-color">CV </g-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,100 @@
+<template>
+  <Layout class="bg-white">
+    <main>
+      <header>
+        <div class="max-w-xl h-screen md:max-w-3xl xl:max-w-4xl mx-auto text-center px-6 py-10 md:py-32 border-b border-gray-300">
+          <div class="w-32 h-32 p-2 border-2 border-gray-400 m-auto rounded-full">
+            <img src="./../../static/images/diwas-dark.jpg" class="w-full h-full rounded-full" alt="">
+          </div>
+          <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">
+            <g-link to="/" class="text-yellow-600">the introvert's diary</g-link>
+          </h1>
+          <p class="text-gray-700 text-lg sm:text-3xl">Thoughts, stories, and ideas.</p>
+        </div>
+      </header>
+      <section>
+        <post-item v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node" />
+      </section>
+      <pagination :info="$page.posts.pageInfo" v-if="$page.posts.pageInfo.totalPages > 1" />
+      <site-footer class="py-8 sm:py-16" />
+    </main>
+  </Layout>
+</template>
+
+<script>
+import config from '~/.temp/config.js'
+import SiteFooter from '@/components/Footer'
+import PostItem from '@/components/PostItem'
+import Pagination from '@/components/Pagination'
+
+export default {
+  components: {
+    PostItem,
+    Pagination,
+    SiteFooter,
+  },
+  metaInfo () {
+    return {
+      title: this.config.siteName,
+      meta: [
+        { property: "og:type", content: 'website' },
+        { property: "og:title", content: this.config.siteName },
+        { property: "og:description", content: this.config.siteDescription },
+        { property: "og:url", content: this.config.siteUrl },
+        { property: "og:image", content: this.ogImageUrl },
+
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: this.config.siteName },
+        { name: "twitter:description", content: this.config.siteDescription },
+        { name: "twitter:site", content: "@cossssmin" },
+        { name: "twitter:creator", content: "@cossssmin" },
+        { name: "twitter:image", content: this.ogImageUrl },
+      ],
+    }
+  },
+  computed: {
+    config () {
+      return config
+    },
+    ogImageUrl () {
+      return `${this.config.siteUrl}/images/bleda-card.png`
+    }
+  },
+}
+</script>
+
+<page-query>
+  query Home ($page: Int) {
+    posts: allPost (page: $page, perPage: 10) @paginate {
+      totalCount
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges {
+        node {
+          id
+          title
+          timeToRead
+          datetime: date (format: "YYYY-MM-DD HH:mm:ss")
+          content
+          excerpt
+          description
+          path
+          cover
+          tags {
+            id
+            title
+            path
+          }
+          author {
+            id
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+</page-query>
+
